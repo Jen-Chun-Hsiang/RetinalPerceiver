06b2da6374b92f9ad4fa3f0d4781d58eb4ba3f1c
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-06 16:41:55 -0600

    update main file for print results

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import torch.optim as optim
 import scipy.io
 import time
 from datasets.simulated_dataset import MatrixDataset
-
+from models.perceiver3d import Perceiver
+import sys
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     savemodel_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/'
+    saveprint_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Prints/'
 
     # Example usage
     dims = [10, 20, 30]  # Size of the matrix
@@ -27,4 +29,22 @@ if __name__ == '__main__':
                 'sample_matrix': sample_matrix, 'output_sum': output_sum,
                 }, filename)
 
-    plt.savefig(f'{p.result_dir}check_figure{cfid}.png')
+    # Example to test the model
+    input_size = 1  # example input size (i.e., channels)
+    output_size = 1  # example output size, e.g., for classification into 10 classes
+
+    model = Perceiver(input_size, 128, output_size, 32, 4, 1, 20, 30, 40, 10)
+    input_tensor = torch.rand(32, 1, 20, 30, 40)  # Example input tensor for n x n image
+    output = model(input_tensor)
+
+    # Check the output shape and values
+    # Save the original stdout
+    original_stdout = sys.stdout
+
+    # Redirect stdout to a file
+    with open(f'{saveprint_dir}model_summary_{timestr}.txt', 'w') as file:
+        sys.stdout = file
+        # Print the summary to file
+        summary(model, (1, 20, 30, 40))
+        print("Output Shape:", output.shape)  # should be [1, output_size]
+        print("Output Values:", output)  # values should be between 0 and 1 due to sigmoid
