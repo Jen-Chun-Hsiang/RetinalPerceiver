bd8d313d9d256e3b5505c9bff143c3a002bbe736
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-06 15:34:19 -0600

    update main file

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+import torchvision.transforms.functional as TF
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import scipy.io
+import time
+from datasets.simulated_dataset import MatrixDataset
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hello World')
+    savemodel_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/'
 
-print('My name?')
+    # Example usage
+    dims = [10, 20, 30]  # Size of the matrix
+    length = 1000  # Number of samples in the dataset
+    target_matrix = np.random.randn(*dims)  # Random target matrix
+    sample_matrix, output_sum = dataset[0]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dataset = MatrixDataset(target_matrix, length)
+    timestr = time.strftime("%Y%m%d-%H%M")
+    filename = f'{savemodel_dir}annmodel_checkpoint_{timestr}.pth'
+    torch.save({'dims': dims, 'length': length, 'target_matrix': target_matrix,
+                'sample_matrix': sample_matrix, 'output_sum': output_sum,
+                }, filename)
