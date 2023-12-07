35e031553262c871625ce974f9ea775c7ecd8380
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-06 20:34:37 -0600

    update main and simulated_dataset files for device consistency

--- datasets/simulated_dataset.py
+++ datasets/simulated_dataset.py
@@ -1,21 +1,21 @@
-import numpy as np
-import time
 import torch
+import numpy as np
 from torch.utils.data import Dataset
-
+import time
 
 class MatrixDataset(Dataset):
-    def __init__(self, target_matrix, length):
+    def __init__(self, target_matrix, length, device):
         """
         Args:
             target_matrix (numpy.ndarray): A target 3D matrix for operations.
             length (int): Number of samples in the dataset.
-            dimensions (tuple): Dimensions (n, m, k) for the randomly generated matrices.
+            device (torch.device): Device where the tensors will be stored.
         """
-        # self.target_matrix = target_matrix
-        self.target_matrix = target_matrix / np.sum(np.abs(target_matrix))  # Normalize the target matrix
+        target_matrix = target_matrix / np.sum(np.abs(target_matrix))  # Normalize the target matrix
+        self.target_matrix = torch.tensor(target_matrix, dtype=torch.float32, device=device)
         self.length = length
         self.dimensions = target_matrix.shape  # Dimensions for the random matrices
+        self.device = device
         self.seed = int(time.time())
 
     def __len__(self):
@@ -23,17 +23,14 @@ class MatrixDataset(Dataset):
 
     def __getitem__(self, idx):
         np.random.seed(self.seed + idx)  # Ensures different random data for each index
+
         # Generate a random n x n matrix from a uniform distribution between 0 and 1
         random_matrix = np.random.uniform(0, 1, self.dimensions)
-        random_matrix = torch.from_numpy(random_matrix).float()
+        random_matrix = torch.tensor(random_matrix, dtype=torch.float32, device=self.device)
         random_matrix = random_matrix.unsqueeze(0)  # Add channel dimension
 
         # Compute the dot product with the target matrix
-        target_tensor = torch.from_numpy(self.target_matrix).float()
-        output_matrix = random_matrix.squeeze(0) * target_tensor
+        output_matrix = random_matrix.squeeze(0) * self.target_matrix
         output_value = output_matrix.sum()
-
-        # Apply sigmoid function to the output_value
-        # output_value = torch.tensor(output_value).clone().detach()
 
         return random_matrix, output_value
--- main.py
+++ main.py
@@ -12,43 +12,49 @@ from models.perceiver3d import Perceiver
 from torchsummary import summary
 import sys
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     savemodel_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/'
     saveprint_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Prints/'
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cuda")
+
+    # Set the device to CUDA if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     # Example usage
     dims = [10, 20, 30]  # Size of the matrix
     length = 1000  # Number of samples in the dataset
     target_matrix = np.random.randn(*dims)  # Random target matrix
 
-    dataset = MatrixDataset(target_matrix, length)
+    # Initialize the dataset with the device
+    dataset = MatrixDataset(target_matrix, length, device)
     sample_matrix, output_sum = dataset[0]
+
+    # Save the dataset information
     timestr = time.strftime("%Y%m%d-%H%M")
     filename = f'{savemodel_dir}annmodel_checkpoint_{timestr}.pth'
     torch.save({'dims': dims, 'length': length, 'target_matrix': target_matrix,
-                'sample_matrix': sample_matrix, 'output_sum': output_sum,
+                'sample_matrix': sample_matrix.cpu().numpy(), 'output_sum': output_sum.item(),
                 }, filename)
 
-    # Example to test the model
-    input_size = 1  # example input size (i.e., channels)
-    output_size = 1  # example output size, e.g., for classification into 10 classes
-
+    # Initialize the model and move it to the device
+    input_size = 1  # Example input size (i.e., channels)
+    output_size = 1  # Example output size, e.g., for classification into 10 classes
     model = Perceiver(input_size, 128, output_size, 32, 4, 1, 20, 30, 40, 10)
-    model = model.to(device)  # Move model to GPU
-    input_tensor = torch.rand(32, 1, 20, 30, 40, device=device)  # Example input tensor for n x n image
+    model.to(device)
+
+    # Create an input tensor and move it to the device
+    input_tensor = torch.rand(32, 1, 20, 30, 40).to(device)  # Example input tensor for n x n image
     output = model(input_tensor)
 
     # Check the output shape and values
-    # Save the original stdout
-    original_stdout = sys.stdout
-
     # Redirect stdout to a file
     with open(f'{saveprint_dir}model_summary_{timestr}.txt', 'w') as file:
         sys.stdout = file
         # Print the summary to file
-        if model.device == output.device:
+        if next(model.parameters()).device == output.device:
             summary(model, (1, 20, 30, 40))
-        print("Output Shape:", output.shape)  # should be [1, output_size]
-        print("Output Values:", output)  # values should be between 0 and 1 due to sigmoid
+        print("Output Shape:", output.shape)  # Should be [1, output_size]
+        print("Output Values:", output)  # Values should be between 0 and 1 due to sigmoid
+
+    # Restore the original stdout
+    sys.stdout = original_stdout
+    print("Model summary has been saved to 'model_summary_{timestr}.txt'.")
