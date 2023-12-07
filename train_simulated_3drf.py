16b4386fb13ba711b7fb98497924fc0be4eb326f
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-07 10:00:40 -0600

    update the parser for perceiver

--- train_simulated_3drf.py
+++ train_simulated_3drf.py
@@ -13,11 +13,25 @@ from utils.utils import save_checkpoint, load_checkpoint
 def parse_args():
     parser = argparse.ArgumentParser(description="Script for Model Training to get 3D RF in simulation")
 
-    parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
+    parser.add_argument('--input_depth', type=int, default=20, help='Number of time points')
+    parser.add_argument('--input_height', type=int, default=30, help='Heights of the input')
+    parser.add_argument('--input_width', type=int, default=40, help='Width of the input')
+    parser.add_argument('--input_channels', type=int, default=1, help='Number of color channel')
+    parser.add_argument('--total_length', type=int, default=1000, help='Number of simulated data')
+    parser.add_argument('--train_proportion', type=float, default=0.8, help='Proportion for training data split')
+    parser.add_argument('--hidden_size', type=int, default=128, help='Number of hidden nodes (information bottleneck)')
+    parser.add_argument('--output_size', type=int, default=1, help='Number of neurons for prediction')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--weight_decay', type=float, default=0.001, help='Weight decay')
     parser.add_argument('--checkpoint_path', type=str, default='./checkpoints/model.pth', help='Path to save the model checkpoint')
     parser.add_argument('--load_checkpoint', action='store_true', help='Flag to load the model from checkpoint')
+
+    parser.add_argument('--num_head', type=int, default=4, help='Number of heads in perceiver')
+    parser.add_argument('--num_iter', type=int, default=1, help='Number of input reiteration')
+    parser.add_argument('--num_latent', type=int, default=16, help='Number of latent length (encoding)')
+    parser.add_argument('--num_band', type=int, default=10, help='Number of bands in positional encoding')
 
     return parser.parse_args()
 
