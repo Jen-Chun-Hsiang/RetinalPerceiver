286ed9fe9967ee71bceb6e48fb876e6075438e7a
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-07 20:28:22 -0600

    debug visualizer

--- visualize_training.py
+++ visualize_training.py
@@ -38,8 +38,8 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     start_epoch, model, optimizer, training_losses, validation_losses = load_checkpoint(checkpoint_path, model, optimizer, device)
-    visualizer_prog.plot_and_save(None, plot_type='plot_line', x_data=training_losses, y_data=validation_losses,
-                                        xlabel='Epochs', ylabel='Loss')
+    #visualizer_prog.plot_and_save(None, plot_type='plot_line', x_data=training_losses, y_data=validation_losses,
+    #                                    xlabel='Epochs', ylabel='Loss')
 
     # Visual evaluation of results
     # Create the target matrix
