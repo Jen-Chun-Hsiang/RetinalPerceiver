4339e5062cb1cf8d00095e551175c0b63bccded5
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2023-12-21 19:28:19 -0600

    visualize cnn with query

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -32,7 +32,7 @@ def main():
     num_latents = 16
     conv3d_out_channels = 10  # default 1
     use_layer_norm = True
-    stimulus_type = 'combo50000tfsfstim123LYnorm1cGelu'
+    stimulus_type = 'cnncombo50000tfsfstim123LYnorm1cGelu'
     model_type = 'RetinalCNN'
     checkpoint_filename = f'PerceiverIO_{time_point}tp{stimulus_type}_checkpoint_epoch_200'
 
