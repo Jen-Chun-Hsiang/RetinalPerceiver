91ec930e03d5d1af1a30daccb4a1ce7e8ad8c8bd
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-01-10 10:45:36 -0600

    Visualize a model

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -25,7 +25,8 @@ def weightedsum_image_plot(output_image_np):
 def main():
     # experiment specific parameters
 
-    stimulus_type = '100ktl123ss2e3c256nl64hs1cpe3kn1st0ps8ag1df'
+    #stimulus_type = '100ktl123ss2e3c256nl64hs1cpe3kn1st0ps8ag1df'
+    stimulus_type = 'cnn100ktl123ss2e3c256nl64hs2ag'
     presented_cell_ids = list(range(32))
     checkpoint_filename = f'PerceiverIO_20tp{stimulus_type}_checkpoint_epoch_200'
 
