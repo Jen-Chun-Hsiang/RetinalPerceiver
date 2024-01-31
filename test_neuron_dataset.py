e8b46a87b1ba0b37d6e541d231aade6da195476f
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-01-31 13:27:11 -0600

    test hdf5 loading

--- test_neuron_dataset.py
+++ test_neuron_dataset.py
@@ -75,7 +75,7 @@ query_array = query_array.to_numpy()
 train_indices, val_indices = train_val_split(len(data_array), chunk_size)
 # get dataset
 train_dataset = RetinalDataset(data_array, query_index, firing_rate_array, image_root_dir, train_indices, chunk_size,
-                               device='cuda', cache_size=80, image_loading_method='pt')
+                               device='cuda', cache_size=80, image_loading_method=image_loading_method)
 
 check_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
 dataiter = iter(check_loader)
