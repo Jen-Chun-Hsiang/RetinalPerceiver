d608e1ea1aee0da016a5476c80da414150e52413
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-02-14 13:44:46 -0600

    visualize

--- visualize_training_neuro.py
+++ visualize_training_neuro.py
@@ -24,7 +24,7 @@ from models.cnn3d import RetinalPerceiverIOWithCNN
 
 def main():
     stimulus_type = '50tpcnn_2024021401_GoodCell2'  # get the name from the check point folder
-    epoch_end = 100  # the number of epoch in the check_point file
+    epoch_end = 40  # the number of epoch in the check_point file
     is_weight_in_label = True # check if the data is good
     is_full_figure_draw = True  # determine whether draw for each neuro or just get stats
     savefig_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Figures/'
