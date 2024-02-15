0cff92c3f7120dd6616429d2df44ea860d59357d
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-02-14 21:20:07 -0600

    visualize

--- check_image_in_ph.py
+++ check_image_in_ph.py
@@ -23,8 +23,8 @@ def compare_images(image1_np, image2_np):
     """Compare two images by computing the correlation coefficient between their pixels."""
     print(f'image1 shape {image1_np.shape}')
     print(f'image2 shape {image2_np.shape}')
-    if image1_np.shape != image2_np.shape:
-        raise ValueError("Images must have the same shape to compare.")
+#    if image1_np.shape != image2_np.shape:
+#        raise ValueError("Images must have the same shape to compare.")
     image1_flat = image1_np.flatten()
     image2_flat = image2_np.flatten()
     corr_matrix = np.corrcoef(image1_flat, image2_flat)
