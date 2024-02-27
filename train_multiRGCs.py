f88ce9fbe34997beb424a0b02383d43df5c6789c
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-02-27 11:53:43 -0600

    debug

--- train_multiRGCs.py
+++ train_multiRGCs.py
@@ -180,14 +180,14 @@ def main():
     query_index = query_index.astype('int64')
     firing_rate_array = firing_rate_array.astype('float32')
 
-
+    '''
     # Save to .mat file
     # filtered_data_mat = {col: filtered_data[col].values for col in filtered_data.columns}
     savemat(os.path.join(savemat_dir, 'train_neuro_list.mat'),
             {"data_array": data_array, "query_array": query_array,
              "query_index": query_index, "firing_rate_array": firing_rate_array})
     raise RuntimeError("Script stopped after saving outputs.")
-
+    '''
 
     # construct the query array for query encoder
     query_df = pd.DataFrame(query_array, columns=['experiment_id', 'neuron_id'])
