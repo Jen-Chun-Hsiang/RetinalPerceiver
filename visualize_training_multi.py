477393fbe671ec6bbcbdef753bd1d90e87284f25
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-02-28 08:49:53 -0600

    debug numpy save

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -299,7 +299,7 @@ def main():
             # Save file
             np.savez(os.path.join(savemat_dir, 'sim_multi_result.npz'),
                      output_image=output_image_np, presented_cell_id=presented_cell_id)
-            savemat(os.path.join(savemat_dir, 'sim_multi_result.npz'),
+            savemat(os.path.join(savemat_dir, 'sim_multi_result.mat'),
                     {"output_image": output_image, "presented_cell_id": presented_cell_id})
             raise RuntimeError("Script stopped after saving outputs.")
 
