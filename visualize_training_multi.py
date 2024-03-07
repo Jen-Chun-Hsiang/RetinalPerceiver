d8bfad9bc6907a67b6e91cab38ce743cc047ed06
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-07 16:42:24 -0600

    visualize permute

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -27,7 +27,7 @@ def weightedsum_image_plot(output_image_np):
 def main():
     stimulus_type = 'SIMContra030301'
     epoch_end = 50
-    is_cross_level = False
+    is_cross_level = True
     is_full_figure_draw = False
     checkpoint_filename = f'PerceiverIO_{stimulus_type}_checkpoint_epoch_{epoch_end}'
 
