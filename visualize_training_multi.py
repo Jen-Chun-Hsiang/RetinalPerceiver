6e3aacd60c314b4dc9fa41e986b814e490a82ab7
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-13 19:32:36 -0500

    typo

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -26,8 +26,8 @@ def weightedsum_image_plot(output_image_np):
 
 def main():
     stimulus_type = 'SIMContra031001'
-    epoch_end = 40
-    is_cross_level = True
+    epoch_end = 20
+    is_cross_level = False
     is_full_figure_draw = False
     checkpoint_filename = f'PerceiverIO_{stimulus_type}_checkpoint_epoch_{epoch_end}'
 
