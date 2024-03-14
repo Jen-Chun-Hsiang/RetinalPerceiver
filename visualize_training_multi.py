c35d0d8001353b9274a0bbae35b7dd7e719a7190
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-14 12:30:32 -0500

    Visualize

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -25,9 +25,9 @@ def weightedsum_image_plot(output_image_np):
     plt.ylabel("Height")
 
 def main():
-    stimulus_type = 'SIMContra031001'
+    stimulus_type = 'SIMContra031101'
     epoch_end = 20
-    is_cross_level = True
+    is_cross_level = False
     is_full_figure_draw = False
     checkpoint_filename = f'PerceiverIO_{stimulus_type}_checkpoint_epoch_{epoch_end}'
 
