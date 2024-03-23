0f6cda28a416ba02e7f2e7e7b3925bc650fb688c
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-23 16:16:56 -0500

    debug set cudnn benchmark as default True

--- train_styleRGCs.py
+++ train_styleRGCs.py
@@ -171,7 +171,7 @@ def main():
     # If CUDA is available, continue with the rest of the script
     device = torch.device("cuda")
     torch.cuda.empty_cache()
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.benchmark = False
     device_id = 0
     torch.cuda.set_device(device_id)
 
