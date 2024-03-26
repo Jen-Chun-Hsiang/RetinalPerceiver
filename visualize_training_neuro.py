2b16156cc1d4a30575936613756854b4e54f4c13
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-26 08:44:28 -0500

    visualize

--- visualize_training_neuro.py
+++ visualize_training_neuro.py
@@ -26,11 +26,11 @@ from models.FiLM3d import FiLMCNN
 
 
 def main():
-    stimulus_type = 'FiLM_2024032202_GoodCell2'  # get the name from the check point folder
+    stimulus_type = 'FiLM_2024032401_GoodCell2'  # get the name from the check point folder
     epoch_end = 160  # the number of epoch in the check_point file
     total_length = 10000
     initial_size = (10, 24, 32)
-    is_original_dataset = True  # use original training data (True) or use the white noise generator (False)
+    is_original_dataset = False  # use original training data (True) or use the white noise generator (False)
     is_encoding_query = False  # whether SeriesEncode was applied (or default embedding)
     is_weight_in_label = False  # check if the data is good
     is_full_figure_draw = True  # determine whether draw for each neuro or just get stats
