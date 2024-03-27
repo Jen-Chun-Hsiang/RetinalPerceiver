f60a44ce01bcb19ba6c796370ad7087c96b05011
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-27 13:52:17 -0500

    visualize debug

--- utils/training_procedure.py
+++ utils/training_procedure.py
@@ -363,7 +363,7 @@ def forward_model(model, dataset, query_array=None, batch_size=16,
                     assert neuron_ids.size(0) == batch_size
                     weights, _, _ = model(images, dataset_ids, neuron_ids)
                     if logger is not None:
-                        logger(f'Finish {batch_idx}/{len(dataloader)} \n')
+                        logger.info(f'Finish {batch_idx}/{len(dataloader)} \n')
                 else:
                     query_vectors = query_array_tensor.repeat(batch_size, 1, 1).to(images.device)
                     # print(f'query_vector shape: {query_vectors.shape}')
