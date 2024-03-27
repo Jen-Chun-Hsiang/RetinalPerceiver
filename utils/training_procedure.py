cb29b262247e758aa782018631105c30b9a7d27d
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-03-27 14:30:52 -0500

    visualize debug

--- utils/training_procedure.py
+++ utils/training_procedure.py
@@ -364,10 +364,10 @@ def forward_model(model, dataset, query_array=None, batch_size=16,
                     dataset_ids = query_vectors[:, 0].to(images.device)
                     neuron_ids = query_vectors[:, 3].to(images.device)
                     matrix_indices = matrix_indices.to(images.device)
-                    assert neuron_ids.size(0) == images.size(0)
-                    assert neuron_ids.size(0) == batch_size
+                    # assert neuron_ids.size(0) == images.size(0)
+                    # assert neuron_ids.size(0) == batch_size
                     weights, _, _ = model(images, dataset_ids, neuron_ids)
-                    mask = (matrix_indices == neuron_ids)
+                    mask = (matrix_indices == neuron_ids).to(images.device)
                     weights = weights[mask]
                     labels = labels[mask]
                     # if logger is not None:
