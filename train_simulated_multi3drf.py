36f3d3a0095e68c03c74e6779dec61e4904d34f3
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-06-05 16:10:05 -0500

    debug

--- train_simulated_multi3drf.py
+++ train_simulated_multi3drf.py
@@ -308,7 +308,7 @@ def main():
             save_checkpoint(epoch, model, optimizer, args, training_losses, validation_losses,
                             validation_contra_losses,
                             os.path.join(savemodel_dir, checkpoint_filename))
-        raise RuntimeError("Stop here for inspection")
+
     if args.parallel_processing:
         # Clean up
         dist.destroy_process_group()
