7a10ff70003ee6f608855549131ce28c0ab601f5
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-06-09 18:32:15 -0500

    debug

--- configs/neuros/neuro_exp1_3cell_041324.py
+++ configs/neuros/neuro_exp1_3cell_041324.py
@@ -22,7 +22,7 @@ experiment_neuron_table['quality'] = experiment_neuron_table['quality'].astype('
 filtered_data = filter_and_merge_data(
         experiment_session_table, experiment_neuron_table,
         selected_experiment_ids=[1],
-        selected_stimulus_types=[1, 2],  # 3
+        selected_stimulus_types=[1, 2, 3],  # 3
         excluded_session_table=None,
         excluded_neuron_table=None,
         included_session_table=None,
