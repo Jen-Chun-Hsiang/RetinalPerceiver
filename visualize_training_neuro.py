40df8023bdc27cbc3531b965983c5bc9c923f672
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-06-12 14:35:07 -0500

    Visualize

--- visualize_training_neuro.py
+++ visualize_training_neuro.py
@@ -34,8 +34,8 @@ def main():
     is_encoding_query = True  # whether SeriesEncode was applied (or default embedding)
     is_weight_in_label = False  # check if the data is good
     is_full_figure_draw = True  # determine whether draw for each neuro or just get stats
-    is_test_dataset = False
-    test_config_name = None  # 'neuro_exp1_3cell_041324_test_12'
+    is_test_dataset = True
+    test_config_name = 'neuro_exp1_3cell_041324_test_12'  # 'neuro_exp1_3cell_041324_test_12' None
     is_use_matrix_index = True
     savefig_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Figures/'
     saveprint_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Prints/'
