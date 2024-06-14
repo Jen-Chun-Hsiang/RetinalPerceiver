d467ab965af8bfd3a43c91916814b9ef238c2538
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-06-14 08:41:09 -0500

    visualize

--- visualize_training_neuro.py
+++ visualize_training_neuro.py
@@ -26,7 +26,7 @@ from models.FiLM3d import FiLMCNN
 
 
 def main():
-    stimulus_type = 'PlugIn_2024061101-v0.8.2_GoodCell3'  # get the name from the check point folder
+    stimulus_type = 'PlugIn_2024061201-v0.8.2_GoodCell3'  # get the name from the check point folder
     epoch_end = 200  # the number of epoch in the check_point file
     total_length = 10000
     initial_size = (10, 24, 32)
@@ -34,8 +34,8 @@ def main():
     is_encoding_query = True  # whether SeriesEncode was applied (or default embedding)
     is_weight_in_label = False  # check if the data is good
     is_full_figure_draw = True  # determine whether draw for each neuro or just get stats
-    is_test_dataset = True
-    test_config_name = 'neuro_exp1_3cell_041324_test_12'  # 'neuro_exp1_3cell_041324_test_12' None
+    is_test_dataset = False
+    test_config_name = None  # 'neuro_exp1_3cell_041324_test_12' None
     is_use_matrix_index = True
     savefig_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Figures/'
     saveprint_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/RetinalPerceiver/Results/Prints/'
