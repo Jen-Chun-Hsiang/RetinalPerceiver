d3405364ee3d857ada5b211e5ddb5021142a9988
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-08-05 15:43:45 -0500

    debug

--- datasets/neuron_dataset.py
+++ datasets/neuron_dataset.py
@@ -522,7 +522,7 @@ class DataConstructor:
             # np.save(session_data_path, session_data)
             # np_ninja.from_ndarray(session_data_path, session_data)
             z = zarr.save(session_data_path, mode='w', shape=session_data.shape, dtype='i4')
-            z[:] = session_data
+            z[:, :] = session_data
 
             np.save(session_fr_path, session_fr_data)
 
