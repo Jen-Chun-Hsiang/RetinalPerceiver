e7ef018202e241feaa2e033baf90c8f7f19d6464
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-08-13 14:03:16 -0500

    debug

--- datasets/neuron_dataset.py
+++ datasets/neuron_dataset.py
@@ -160,9 +160,9 @@ class RetinalDataset(Dataset):
                     images_3d[i] = image
         else:
             # Load each unique image once and assign to images_3d
-            for unique_frame_id in unique_frame_ids:
+            for unique_idx, unique_frame_id in enumerate(unique_frame_ids):
                 image = self.load_image(experiment_id, session_id, unique_frame_id)
-                indices = np.where(inverse_indices == unique_frame_id)[0]
+                indices = np.where(inverse_indices == unique_idx)[0]
                 repeated_image = image.unsqueeze(0).repeat(len(indices), 1, 1, 1)  # Repeat image across new batch dimension
                 images_3d[indices] = repeated_image
                 # for i in indices:
