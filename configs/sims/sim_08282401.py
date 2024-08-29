34fe4b7dbad1e10b4311360547034f5873dd21df
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-08-28 21:00:40 -0500

    debug

--- configs/sims/sim_08282401.py
+++ configs/sims/sim_08282401.py
@@ -1,7 +1,7 @@
 from utils.interact_data import fill_blanks_from_excel
 import pandas as pd
 
-sim_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/VideoSpikeDataset/ExperimentSheets.xlsx'
+sim_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/VideoSpikeDataset/DisentangleTestingSheets.xlsx'
 
 query_table = fill_blanks_from_excel(sim_dir, sheet_name='Standard', cell_range='B3:D30')
 tf_param_table = pd.read_excel(sim_dir, sheet_name='TF_params', usecols='A:G')
