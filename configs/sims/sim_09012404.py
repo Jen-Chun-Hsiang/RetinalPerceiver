9fb98d3785c87c458caf859e0337ac276ce8c08b
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-09-04 20:09:39 -0500

    new experiment

---
+++ configs/sims/sim_09012404.py
@@ -0,0 +1,8 @@
+from utils.interact_data import fill_blanks_from_excel
+import pandas as pd
+
+sim_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/VideoSpikeDataset/DisentangleTestingSheets.xlsx'
+
+query_table = fill_blanks_from_excel(sim_dir, sheet_name='Standard', cell_range='N3:P70')
+tf_param_table = pd.read_excel(sim_dir, sheet_name='TF_params', usecols='A:G')
+sf_param_table = pd.read_excel(sim_dir, sheet_name='SF_params', usecols='A:J')
