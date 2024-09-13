785578ef927e168b20426133060f2488f77c3166
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-09-13 15:09:47 -0500

    new experiment

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -26,8 +26,8 @@ def weightedsum_image_plot(output_image_np):
     plt.ylabel("Height")
 
 def main():
-    stimulus_type = 'SIMPlugIn_08302402'
-    epoch_end = 150
+    stimulus_type = 'SIMPlugIn_09082404'
+    epoch_end = 100
     is_cross_level = False
     is_full_figure_draw = False
     checkpoint_filename = f'PerceiverIO_{stimulus_type}_checkpoint_epoch_{epoch_end}'
