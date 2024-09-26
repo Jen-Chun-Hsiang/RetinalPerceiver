1e0a3569612a701718bb3f6207c5aaa2f798b820
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-09-26 17:23:58 -0500

    debug

--- utils/training_procedure.py
+++ utils/training_procedure.py
@@ -102,7 +102,11 @@ class Trainer:
 
         num_batches = query_vectors.shape[0]
         mask = torch.rand(num_batches) < self.masking_prob
-        query_vectors[mask, :, self.masking_pos] = -1
+        # Apply the mask
+        selected_batches = mask.nonzero(as_tuple=True)[0]
+        if selected_batches.numel() > 0:
+            expanded_mask = selected_batches[:, None, None].expand(-1, query_vectors.shape[1], len(self.masking_pos))
+            query_vectors[expanded_mask, :, self.masking_pos] = -1
         print(f'self.masking_pos: {self.masking_pos}')
         print(f'mask: {mask}')
         print(f'query_vectors type: {type(query_vectors)}')
