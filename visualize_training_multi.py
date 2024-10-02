e7e5ff53a22b6960a2670e146ddd42adb34521bd
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-02 17:34:54 -0500

    debug

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -89,7 +89,7 @@ def main():
     parameter_generator = ParameterGenerator(sf_param_table, tf_param_table)
     param_lists, series_ids = parameter_generator.generate_parameters(query_table)
     syn_param_lists = parameter_generator.generate_parameters_from_query_list(series_ids)
-
+    print(f'syn_param_list type: {type(syn_param_lists)}')
 
     # Save to .mat file
     savemat(os.path.join(savemat_dir, 'sim_multi_list_10022401.mat'),
