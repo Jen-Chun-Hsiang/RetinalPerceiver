2cfedca099851bb4e4b2338946d792b1bd5fe3e0
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-02 18:31:56 -0500

    debug

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -89,9 +89,10 @@ def main():
     parameter_generator = ParameterGenerator(sf_param_table, tf_param_table)
     param_lists, series_ids = parameter_generator.generate_parameters(query_table)
     syn_param_lists = parameter_generator.generate_parameters_from_query_list(series_ids)
-    syn_param_lists = [x if x is not None else np.nan for x in syn_param_lists]
+    # syn_param_list = np.array(syn_param_list, dtype=float)
+    # syn_param_lists = [x if x is not None else np.nan for x in syn_param_lists]
     print(f'syn_param_list type: {type(syn_param_lists)}')
-    print(f'series_ids type: {type(series_ids)}')
+    print(f'param_list type: {type(param_list)}')
 
     # Save to .mat file
     savemat(os.path.join(savemat_dir, 'sim_multi_list_10022401.mat'),
