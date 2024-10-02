bccd4756695f0649db0b1a2858948c3494d4342e
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-02 18:36:19 -0500

    debug

--- visualize_training_multi.py
+++ visualize_training_multi.py
@@ -92,7 +92,7 @@ def main():
     # syn_param_list = np.array(syn_param_list, dtype=float)
     # syn_param_lists = [x if x is not None else np.nan for x in syn_param_lists]
     print(f'syn_param_list type: {type(syn_param_lists)}')
-    print(f'param_list type: {type(param_list)}')
+    print(f'param_lists type: {type(param_lists)}')
 
     # Save to .mat file
     savemat(os.path.join(savemat_dir, 'sim_multi_list_10022401.mat'),
