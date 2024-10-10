74ff6b3dfde704b5520d0c67f96cc592b02767dc
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-10 11:20:09 -0500

    debug

--- configs/sims/sim_09012403.py
+++ configs/sims/sim_09012403.py
@@ -6,3 +6,9 @@ sim_dir = '/storage1/fs1/KerschensteinerD/Active/Emily/RISserver/VideoSpikeDatas
 query_table = fill_blanks_from_excel(sim_dir, sheet_name='Standard', cell_range='J3:L70')
 tf_param_table = pd.read_excel(sim_dir, sheet_name='TF_params', usecols='A:G')
 sf_param_table = pd.read_excel(sim_dir, sheet_name='SF_params', usecols='A:J')
+
+# Encode series_ids into query arrays
+max_values = {'Experiment': 100, 'Type': 100}
+encoding_type = {'Experiment': 'uniform', 'Type': 'uniform', 'Coord_x': 'extend', 'Coord_y': 'extend'}
+lengths = {'Experiment': 6, 'Type': 6, 'Coord_x': 1, 'Coord_y': 1}
+shuffle_components = None
