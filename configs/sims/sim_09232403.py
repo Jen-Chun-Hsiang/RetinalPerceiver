f6603f13c898c9ff5e4894c6ae466d7c908306b7
Jen-Chun-Hsiang <jenchunhsiang@gmail.com>
2024-10-11 10:07:39 -0500

    get data

--- configs/sims/sim_09232403.py
+++ configs/sims/sim_09232403.py
@@ -8,7 +8,6 @@ tf_param_table = pd.read_excel(sim_dir, sheet_name='TF_params', usecols='A:G')
 sf_param_table = pd.read_excel(sim_dir, sheet_name='SF_params', usecols='A:J')
 
 max_values = {'Experiment': 100, 'Type': 100}
-# skip_encoding = {'Experiment': False, 'Type': False, 'Coord_x': True, 'Coord_y': True}
 encoding_type = {'Experiment': 'uniform', 'Type': 'uniform',  'Coord_x': 'extend', 'Coord_y': 'extend'}
 lengths = {'Experiment': 6, 'Type': 6, 'Coord_x': 1, 'Coord_y': 1}
 shuffle_components = None
