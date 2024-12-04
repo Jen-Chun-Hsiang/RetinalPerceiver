86576b076202fe72bae2450226edde5d78b9374d
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-12-04 11:16:29 -0600

    do not train worker

--- train_multiRGCs.py
+++ train_multiRGCs.py
@@ -331,7 +331,7 @@ def main():
             data_array, query_index, firing_rate_array, image_root_dir,
             cache_size=args.cache_size, image_loading_method=args.image_loading_method
         )
-        train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0)
+        train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
         save_distributions(train_loader, n=n, folder_name=savefig_dir, file_name=plot_file_name)
 
     else:
