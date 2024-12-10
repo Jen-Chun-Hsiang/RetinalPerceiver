c2a1f9ccf18030931ce7ace4843bd297ad22d954
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-12-10 12:10:29 -0600

    check size more

--- models/cnn3d.py
+++ models/cnn3d.py
@@ -191,7 +191,7 @@ class RetinalPerceiverIOWithCNN(nn.Module):
         query_array = query_array
         query_array = query_array.repeat(1, self.num_latents, 1)
         query_array = add_gradient(query_array, dim=1, start=-1, end=1)
-
+        print(f"Size of input_array: {input_array.size()}")
         cnn_output = self.front_end_cnn(input_array)
         print(f"Size of cnn_output: {cnn_output.size()}")
         # Apply positional encoding
