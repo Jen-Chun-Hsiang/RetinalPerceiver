d2ea24f320a63ef3af2558c33f96c01c9f732e15
Emily Hsiang <44245660+Jen-Chun-Hsiang@users.noreply.github.com>
2024-12-10 10:27:57 -0600

    remove device from simulated dataset

--- datasets/simulated_dataset.py
+++ datasets/simulated_dataset.py
@@ -11,22 +11,21 @@ import matplotlib.pyplot as plt
 
 
 class MatrixDataset(Dataset):
-    def __init__(self, target_matrix, length, device, combination_set=None, ratio_for_one=0.5,
+    def __init__(self, target_matrix, length, combination_set=None, ratio_for_one=0.5,
                  initial_size=None):
         """
         Args:
             target_matrix (numpy.ndarray or torch.Tensor): A target 3D matrix for operations.
             length (int): Number of samples in the dataset.
-            device (torch.device): Device where the tensors will be stored.
             combination_set (list): Set of matrix types to use for generating random matrices.
             ratio_for_one (float): Ratio for generating binary matrices (Type 2).
         """
 
         # Convert target_matrix to a PyTorch tensor
         if isinstance(target_matrix, np.ndarray):
-            target_matrix = torch.tensor(target_matrix, dtype=torch.float32, device=device)
+            target_matrix = torch.tensor(target_matrix, dtype=torch.float32)
         elif isinstance(target_matrix, torch.Tensor):
-            target_matrix = target_matrix.to(device)
+            target_matrix = target_matrix
 
         # Normalize the target matrix
         norm_factor = torch.sum(torch.abs(target_matrix))
@@ -34,7 +33,6 @@ class MatrixDataset(Dataset):
 
         self.length = length
         self.dimensions = target_matrix.shape
-        self.device = device
         self.seed = int(time.time())
         self.ratio_for_one = ratio_for_one
         self.combination_set = combination_set if combination_set is not None else [1]
@@ -63,19 +61,19 @@ class MatrixDataset(Dataset):
             if self.initial_size is None:
                 self.initial_size = self.dimensions
 
-            initial_noise = torch.rand((1, *self.initial_size), device=self.device)
+            initial_noise = torch.rand((1, *self.initial_size))
             return F.interpolate(initial_noise.unsqueeze(0), size=self.dimensions, mode='nearest').squeeze()
         elif matrix_type == 2:  # random 0 and 1
             p = self.ratio_for_one
             if not 0 <= p <= 1:
                 raise ValueError("Threshold probability p must be between 0 and 1")
-            return torch.bernoulli(p * torch.ones(self.dimensions, device=self.device))
+            return torch.bernoulli(p * torch.ones(self.dimensions))
         elif matrix_type == 3:  # random contrast full field
-            base = torch.rand(self.dimensions[0], device=self.device)
+            base = torch.rand(self.dimensions[0])
             replicated_base = base.unsqueeze(1).unsqueeze(2).expand(*self.dimensions)
             return replicated_base
         elif matrix_type == 4:  # random spatial no temporal
-            base = torch.rand(1, 1, self.dimensions[1], self.dimensions[2], device=self.device)
+            base = torch.rand(1, 1, self.dimensions[1], self.dimensions[2])
             base = self.median_filter(base)
             # Corrected expansion along the second dimension
             replicated_base = base.repeat(1, self.dimensions[0], 1, 1)
@@ -144,14 +142,13 @@ class SharpenFilter(nn.Module):
         return x
 
 class MultiMatrixDataset(MatrixDataset):
-    def __init__(self, target_matrices, length, device, combination_set=None, ratio_for_one=0.5,
+    def __init__(self, target_matrices, length, combination_set=None, ratio_for_one=0.5,
                  add_noise=False, noise_level=0.1, use_relu=False, output_offset=0.12,
                  initial_size=(20, 20, 24)):
         """
         Args:
                     target_matrices (numpy.ndarray or torch.Tensor): A 4D matrix.
                     length (int): Number of samples in the dataset.
-                    device (torch.device): Device where the tensors will be stored.
                     combination_set (list): Set of matrix types to use for generating random matrices.
                     ratio_for_one (float): Ratio for generating binary matrices (Type 2).
                     add_noise (bool): Whether to add noise to the output values.
@@ -161,14 +158,14 @@ class MultiMatrixDataset(MatrixDataset):
 
         # Ensure target_matrices is a torch tensor
         if isinstance(target_matrices, np.ndarray):
-            target_matrices = torch.tensor(target_matrices, dtype=torch.float32, device=device)
+            target_matrices = torch.tensor(target_matrices, dtype=torch.float32)
         elif isinstance(target_matrices, torch.Tensor):
-            target_matrices = target_matrices.to(device)
+            target_matrices = target_matrices
         else:
             raise ValueError("target_matrices must be either a numpy array or a torch tensor")
 
         # Initialize the base MatrixDataset with the first matrix (as a placeholder)
-        super().__init__(target_matrices[0], length, device, combination_set, ratio_for_one, initial_size)
+        super().__init__(target_matrices[0], length, combination_set, ratio_for_one, initial_size)
 
         # Normalize and store each matrix in the first dimension of target_matrices
         self.target_matrices = []
@@ -201,7 +198,7 @@ class MultiMatrixDataset(MatrixDataset):
 
         # Add noise to output value if required
         if self.add_noise:
-            noise = torch.randn((), device=self.device) * self.noise_level
+            noise = torch.randn(1).item() * self.noise_level
             output_value += noise
 
         # Apply sigmoid function to output value if required
